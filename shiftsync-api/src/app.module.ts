2ce8ad809c652510a6bf92c9527dd2269e8192b0
michaeljogoh-maker <michael.jogoh@texsphere.co>
2026-03-15 14:48:01 +0100

    feat: shiftsync-api codebase setup

---
+++ shiftsync-api/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { LocationsModule } from './modules/locations/locations.module';
+import { SkillsModule } from './modules/skills/skills.module';
+import { AvailabilityModule } from './modules/availability/availability.module';
+import { ShiftsModule } from './modules/shifts/shifts.module';
+import { AssignmentsModule } from './modules/assignments/assignments.module';
+import { SwapsModule } from './modules/swaps/swaps.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { AuditModule } from './modules/audit/audit.module';
+import { AnalyticsModule } from './modules/analytics/analytics.module';
+import { RealtimeModule } from './modules/realtime/realtime.module';
+import { JobsModule } from './modules/jobs/jobs.module';
+import { MailModule } from './modules/mail/mail.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './modules/database/database.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, LocationsModule, SkillsModule, AvailabilityModule, ShiftsModule, AssignmentsModule, SwapsModule, NotificationsModule, AuditModule, AnalyticsModule, RealtimeModule, JobsModule, MailModule, CommonModule, ConfigModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
